d854899cb97e8075c6cec02a1b8cc8e5bcd9114c
DESKTOP-O3TUH4V\User <nazar.polyvanchuk@gmail.com>
2019-06-17 01:27:59 +0300

    Finaly version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// HTML animation
 var hotbod = document.querySelector("body");
 
 function doStuff() {
@@ -7,8 +8,9 @@ function doStuff() {
 window.onload = function() {
     doStuff();
 };
-
 // -----
+
+// Burger menu
 (function($){
     $(function() {
       $('.menu__icon').on('click', function() {
@@ -16,4 +18,34 @@ window.onload = function() {
       });
     });
   })(jQuery);
+// -----
+
+// Go Top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// -----
+
+//Slow Scroll Menu
+$(document).ready(function(){
+      $("#menu__links").on("click","a", function (event) {
+          event.preventDefault();
+          var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+          $('body,html').animate({scrollTop: top}, 1500);
+      });
+  });
+// -----
+  
 
